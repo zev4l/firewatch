6982a482d40fcdcb476a71afd63bd0fde57119b6
Ha-sake <57680069+Ha-sake@users.noreply.github.com>
2020-11-14 22:49:07 +0000

    Initial structuring according to instructions

--- firewatch/src/Instituicao.java
+++ firewatch/src/Instituicao.java
@@ -1,3 +1,66 @@
+import java.util.Calendar;
+import java.util.List;
+
+import Regiao.EstadoSimulacao;
+import Regiao.NivelPerigo; 
+//FIXME: Por alguma razão estes imports não funcionam, apesar de terem sido literalmente sugeridos pelo VSCode.
+
+
 public class Instituicao {
+
+    // Relação entre o número de anos que passaram desde o último fogo e o nível de perigo 
+    public static final int[] RISCO_ANOS = {2, 3, 5, 8}; 
+
+    // Relação entre a força do vento e o número de elementos vizinhos no ambiente de uma
+    // região que serão afetados
+    public static final int[] VENTOS_LIMITES = {0, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21};
+
+    private String designacao;
+
+
+    public Instituicao(String designacao) {
+        this.designacao = designacao;
+    }
+
+    public void adicionaRegiao(String nome, Calendar ultFogo, int largura, int altura,
+                                List<Par<Integer, Integer>> casas,
+                                List<Par<Integer, Integer>> estradas,
+                                List<Par<Integer, Integer>> agua) {
+
+        // Assumindo que ainda não existe uma região com este nome, 
+        //e que os dados são válidos, cria-a com os parãmetros referidos 
+
     
+    }
+
+    public boolean existeRegiao(String nome) {
+        // Devolve true se a região estiver registada nesta instituição
+    }
+
+    public List<Par<String,NivelPerigo>> niveisDePerigo() {
+        // Devolve os nomes e níveis de perigo ed cara região registada,
+        // os níveis devem ser calculados relativamente à data corrente
+        // e À constante RISCO_ANOS
+    }
+
+    public EstadoSimulacao[][] alvoSimulacao() {
+        // Devolve alvo da simulação da região de maior nível de perigo
+    }
+
+    public boolean podeAtuar() {
+        // Devolve true se existe pelo menos uma região com elementos ardíveis
+    }
+
+    public void registaFogo(String regiao, Calendar data, List<Par<Integer, Integer>> sitios) {
+        // regista na região referida um fogo acontecido na data referida. 
+        // ATENÇÃO: APÓS ISTO PODE ACONTECER QUE A REGIÃO DE MAIOR NIVEL DE PERIGO MUDE.
+    }
+
+    public String toString() {
+        // Self-explanatory
+    }
+
+    // Podemos fazer mais métodos que estes, desde que sejam privados
+
+
 }
--- firewatch/src/Regiao.java
+++ firewatch/src/Regiao.java
@@ -1,3 +1,71 @@
+import java.util.Calendar;
+import java.util.List;
+
 public class Regiao {
+    private String nome;
+    private Calendar ultFogo;
+    private int [][] ambiente;
     
+    public Regiao (String nome, Calendar ultFogo, int largura, int altura, List<Par<Integer, Integer>> casas, List<Par<Integer, Integer>> estradas, List<Par<Integer, Integer>> agua) {
+        this.nome = nome;
+        this.ultFogo = ultFogo;
+        this.ambiente = new int[largura][altura];
+        // ... TODO: Falta adicionar o resto dos atributos
+
+        
+    }
+    
+
+    public String nome() {
+        return this.nome;
+    }
+
+    public int ardiveis() {
+        // Devolve número de casas + terrenos ainda não ardidos
+    }
+
+    public void registaFogo(Calendar data, List<Par<Integer, Integer>> sitios) {
+        // Regista um novo fogo para a região, acontecido na data data, em que arderam
+        // os elementos referidos em sitios
+    }
+
+    public static boolean dadosValidos(int largura, int altura, 
+                                        List<Par<Integer, Integer>> casas,
+                                        List<Par<Integer, Integer>> estradas,
+                                        List<Par<Integer, Integer>> agua) {
+        // veririfica se os indices dados em casas, estradas e agua condizem com a largura e altura dadas,
+        // (ver se para cada List<Par<Integer, Integer>> .primeiro() < largura e .segundo() < altura)
+    }
+
+    public EstadoSimulacao[][] alvoSimulacao() {
+        // devolve matriz correspondente ao ambiente atual em que os terrenos e casas não ardidos são EstadoSimulacao.LIVRE
+        // e a água, estradas e elementos já ardidos são EstadoSimulacao.OBSTACULO
+    }
+
+    public NivelPerigo nivelPerigo(Calendar data, int[] tempoLimites) {
+        // retorna o nível de perigo, ver forma de cálculo no início da pag.4 do enunciado
+    }
+
+    public String toString() {
+        // self-explanatory
+    }
+
+    // Defini aqui os enums, apesar de sentir que deviam estar
+    // no Simulador.java, porque não sei se o podemos editar.
+
+    enum EstadoSimulacao {
+        LIVRE,
+        AFETADO,
+        OBSTACULO
+    }
+
+    enum NivelPerigo {
+        VERDE,
+        AMARELO,
+        LARANJA,
+        VERMELHO
+    }
+
+    // Podemos fazer mais métodos que estes, desde que sejam privados
+
 }
